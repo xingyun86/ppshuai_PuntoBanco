5bad2996375fd645b0b767bad337d3fd39a899c8
xingyun86 <xingyun_ppshuai@yeah.net>
2020-01-17 00:30:16 +0800

    提交刻度表绘制

--- src/PuntoBanco/RotateDialogWindow.h
+++ src/PuntoBanco/RotateDialogWindow.h
@@ -199,7 +199,6 @@ void DrawPie(Gdiplus::Graphics* pGraphics, HWND hWnd) {
     clr.SetFromCOLORREF(m_ColorBack);
     pBrush->SetColor(clr);
     pGraphicsTmp->FillEllipse(pBrush, rcIn);
-    
     //
     pGraphics->DrawImage(targBmp,
         Gdiplus::RectF(0, 0, targBmp->GetWidth(), targBmp->GetHeight()),
@@ -244,7 +243,7 @@ void DrawPie(Gdiplus::Graphics* pGraphics, HWND hWnd) {
         {
             m_AnimationIndex = 0;
             AnimationItem item = m_AnimationArray[ANIMATION_COUNTSIZE - 1];
-            for (int i = ANIMATION_COUNTSIZE - 1; i > 0; i--)
+            for (int i = ANIMATION_COUNTSIZE - 1; i >= 0; i--)
             {
                 m_AnimationArray[i] = m_AnimationArray[i - 1];
             }
@@ -344,6 +343,194 @@ void DrawWordArt(Gdiplus::Graphics* pGraphics)
     pGraphics->DrawImage(&bitmap,
         Gdiplus::RectF(0, 200, bitmap.GetWidth(), bitmap.GetHeight()),
         0, 0, bitmap.GetWidth(), bitmap.GetHeight(), Gdiplus::Unit::UnitPixel);
+}
+////////////////////////////////////////////////////////////////////////////////
+//
+Gdiplus::SizeF GetTextBounds(const Gdiplus::Font& font, const Gdiplus::StringFormat& strFormat, const CString& szText)
+{
+    Gdiplus::RectF rcBound;
+    Gdiplus::FontFamily fontFamily;
+    Gdiplus::GraphicsPath graphicsPath;
+    font.GetFamily(&fontFamily);
+    graphicsPath.AddString(szText, -1, &fontFamily, font.GetStyle(), font.GetSize(), \
+        Gdiplus::PointF(0, 0), &strFormat);
+    /// 获取边界范围
+    graphicsPath.GetBounds(&rcBound);
+    /// 返回文本的宽高
+    return Gdiplus::SizeF(rcBound.Width, rcBound.Height);
+}
+Gdiplus::Bitmap* GetPumpImage(float fMin, float fMax, float fValue, CString strUnit)
+{
+    const float PI = 3.141592653;
+    if (fMin > fValue)
+    {
+        fMin = fValue;
+    }
+    if (fValue > fMax)
+    {
+        fMax = fValue;
+    }
+    int nWidth, nHeight;
+    nWidth = nHeight = 200;
+    Gdiplus::Rect rect(0, 0, nWidth, nHeight);
+    int nTable = 5;		//	大格
+    int nScale = 5;		//	每小段里面的小格
+    //	每段颜色
+    Gdiplus::Color TableColor[] =
+    {
+        Gdiplus::Color::Green,
+        Gdiplus::Color::Blue,
+        Gdiplus::Color::Cyan,
+        Gdiplus::Color::Yellow,
+        Gdiplus::Color::Red
+    };
+    //	张开的弧度
+    float fRadian = 60;
+    //	同心圆半径
+    int nRadius[] =
+    {
+        200,
+        160,
+        90
+    };
+    //	圆心
+    Gdiplus::Point ptCenter(nWidth / 2, nHeight + 20);
+    //	刻度线长度
+    int nLongScal = 15;
+    int nShortScal = 10;
+    //	背景色
+    Gdiplus::Color bkColor = Gdiplus::Color::SkyBlue;
+    //	刻度字体,大小,颜色
+    Gdiplus::Color ScaleColor = Gdiplus::Color::Blue;
+    Gdiplus::REAL	ScaleSize = 8;
+    CString ScaleFont = _T("Times New Roman");
+    //CString ScaleFont = _T("宋体");
+
+    Gdiplus::Bitmap* pImage = new Gdiplus::Bitmap(nWidth, nHeight);
+    Gdiplus::Graphics* pGraph = Gdiplus::Graphics::FromImage(pImage);
+    Gdiplus::StringFormat format;
+    //	消除锯齿
+    format.SetAlignment(Gdiplus::StringAlignment::StringAlignmentCenter);
+    format.SetLineAlignment(Gdiplus::StringAlignment::StringAlignmentCenter);
+    pGraph->SetTextRenderingHint(Gdiplus::TextRenderingHint::TextRenderingHintAntiAlias);
+    pGraph->Clear(Gdiplus::Color::Transparent);
+    Gdiplus::Color LineColor = Gdiplus::Color::Chocolate;
+    Gdiplus::Pen LinePen(LineColor, 1);
+    //	画三段弧
+    rect = Gdiplus::Rect(ptCenter.X - nRadius[0], ptCenter.Y - nRadius[0], 2 * nRadius[0], 2 * nRadius[0]);
+    pGraph->DrawArc(&LinePen, rect, 180 + (90 - fRadian / 2), fRadian);
+    rect = Gdiplus::Rect(ptCenter.X - nRadius[1], ptCenter.Y - nRadius[1], 2 * nRadius[1], 2 * nRadius[1]);
+    pGraph->DrawArc(&LinePen, rect, 180 + (90 - fRadian / 2), fRadian);
+    rect = Gdiplus::Rect(ptCenter.X - nRadius[2], ptCenter.Y - nRadius[2], 2 * nRadius[2], 2 * nRadius[2]);
+    pGraph->DrawArc(&LinePen, rect, 180 + (90 - fRadian / 2), fRadian);
+
+    //	标刻度
+
+    //	分段
+    for (int i = 0; i <= nTable * nScale; i++)
+    {
+        Gdiplus::Point p1, p0;
+        double f0 = ((180 - fRadian) / 2 + fRadian / (nTable * nScale) * i) / 180 * PI;
+        p1.X = ptCenter.X + nRadius[1] * cos(f0);
+        p1.Y = ptCenter.Y - nRadius[1] * sin(f0);
+        if (i % nTable == 0)
+        {
+
+            {
+                p0.X = ptCenter.X + (nRadius[1] + nLongScal) * cos(f0);
+                p0.Y = ptCenter.Y - (nRadius[1] + nLongScal) * sin(f0);
+
+                CString str;
+                str.Format(_T("%.0f"), fMax - (fMax - fMin) / (nTable * nScale) * i);
+                //	获取文本所占的大小
+                //CSize size = CGraphCurveView::GetOutPutTextSize(pGraph, str, ScaleFont, ScaleSize);
+                Gdiplus::SizeF size = GetTextBounds(Gdiplus::Font(CStringW(ScaleFont), ScaleSize), format, str);
+                Gdiplus::RectF textrc;
+                textrc.X = p0.X - size.Width / 2;
+                textrc.Y = p0.Y - size.Height;
+                textrc.Width = size.Width;
+                textrc.Height = size.Height;
+                pGraph->DrawString(
+                    str,
+                    -1,
+                    &Gdiplus::Font(ScaleFont, ScaleSize),
+                    textrc,
+                    &format,
+                    &Gdiplus::SolidBrush(ScaleColor)
+                );
+                Gdiplus::Point p2, p3;
+                int nGap = 5;
+                p3.X = ptCenter.X + (nRadius[0] + 0) * cos(f0);
+                p3.Y = ptCenter.Y - (nRadius[0] + 0) * sin(f0);
+                p2.X = ptCenter.X + (nRadius[0] - nGap) * cos(f0);
+                p2.Y = ptCenter.Y - (nRadius[0] - nGap) * sin(f0);
+                pGraph->DrawLine(&LinePen, p2, p3);
+            }
+
+        }
+        else
+        {
+            p0.X = ptCenter.X + (nRadius[1] + nShortScal) * cos(f0);
+            p0.Y = ptCenter.Y - (nRadius[1] + nShortScal) * sin(f0);
+        }
+
+        pGraph->DrawLine(&LinePen, p0, p1);
+    }
+
+    CString strDataFont = _T("Times New Roman");
+    //CString strDataFont = _T("宋体");
+    int DataSize = 8;
+    Gdiplus::Color DataColor = Gdiplus::Color::Red;
+
+    //	单位
+    Gdiplus::RectF UnitRc;
+    
+    //CSize size = CGraphCurveView::GetOutPutTextSize(pGraph, strUnit, strDataFont, DataSize);
+    Gdiplus::SizeF size = GetTextBounds(Gdiplus::Font(CStringW(strDataFont), DataSize), format, strUnit);
+    UnitRc.X = ptCenter.X - size.Width / 2;
+    UnitRc.Y = ptCenter.Y - (nRadius[0] - nRadius[1] + nRadius[2] + size.Height / 2);
+    UnitRc.Width = size.Width;
+    UnitRc.Height = size.Height;
+    pGraph->DrawString(
+        strUnit,
+        -1,
+        &Gdiplus::Font(strDataFont, DataSize),
+        UnitRc,
+        &format,
+        &Gdiplus::SolidBrush(DataColor)
+    );
+
+    //	数据
+    CString strData;
+    strData.Format(_T("%.0f"), fValue);
+    //size = CGraphCurveView::GetOutPutTextSize(pGraph, strData, strDataFont, DataSize);
+    size = GetTextBounds(Gdiplus::Font(CStringW(strDataFont), DataSize), format, strData);
+    Gdiplus::RectF DataRc;
+    DataRc.X = ptCenter.X - size.Width / 2;
+    DataRc.Y = ptCenter.Y - (nRadius[2] - nLongScal);
+    DataRc.Width = size.Width;
+    DataRc.Height = size.Height;
+    pGraph->DrawString(
+        strData,
+        -1,
+        &Gdiplus::Font(strDataFont, DataSize),
+        DataRc,
+        &format,
+        &Gdiplus::SolidBrush(DataColor)
+    );
+
+    //	画指针
+    Gdiplus::Pen PointPen(Gdiplus::Color::Green, 2);
+    Gdiplus::Point p0, p1;
+    double f0 = ((180 - fRadian) / 2 + fRadian / (fMax - fMin) * (-fValue + fMax)) / 180 * PI;
+    p0.X = ptCenter.X + (nRadius[2] * cos(f0));
+    p0.Y = ptCenter.Y - (nRadius[2] * sin(f0));
+    p1.X = ptCenter.X + ((nRadius[1] + nShortScal / 2) * cos(f0));
+    p1.Y = ptCenter.Y - ((nRadius[1] + nShortScal / 2) * sin(f0));
+    pGraph->DrawLine(&PointPen, p0, p1);
+
+    delete pGraph;
+    return pImage;
 }
 /////////////////////////////////////////////////////////////////////////////////
 //
@@ -563,7 +750,16 @@ public:
         
         DrawPie(pGraphicsMemory, hWnd);
         DrawWordArt(pGraphicsMemory);
-
+		Gdiplus::Bitmap * _bitmap = GetPumpImage(0.0f, 100.0f, 50.0f, _T("KN"));
+		if (_bitmap != NULL)
+		{
+            pGraphicsMemory->DrawImage(_bitmap,
+		       Gdiplus::RectF(150, 0, _bitmap->GetWidth(), _bitmap->GetHeight()),
+		       0, 0, _bitmap->GetWidth(), _bitmap->GetHeight(), Gdiplus::Unit::UnitPixel);
+		   delete _bitmap;
+		   _bitmap = NULL;
+		}
+				   
         HWND btnWnd = GetDlgItem(hWnd, IDOK);
         RECT rcBtn = { 0 };
         RECT rcWnd = { 0 };
